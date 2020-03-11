0bb52fdcfb5d0815f0ad5284b0cc5f399d379c41
Abdella Shukur <abdella.shukur2@gmail.com>
2020-03-10 21:02:52 -0400

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var renderMovies = function renderMovies(movieArray) {
+        // Creating a variable with a .map method (methods by default are always functions)
+        moviesHTML = movieArray.map(function(currentMovie) {
+            
+        return  `
+        <div>
+        <img src="${currentMovie.Poster}" width="150"/>
+        <h3>${currentMovie.Title}</h3>
+        <p>${currentMovie.Year}</p>
+        </div>
+        ` 
+    
+       }).join("")
+       return moviesHTML;
+    }
+
+       var renderData = function renderData() { 
+           document.getElementById("moviesContainer").innerHTML = renderMovies(movieData) } 
+         
+ 
+// END OF RENDER MOVIES FUNCTION
+renderData();
+
+console.log(moviesHTML)
+
+
+})
