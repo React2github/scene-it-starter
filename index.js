d0aa50cdb93deefc84af128186d7f44c2c3fa488
Abdella Shukur <abdella.shukur2@gmail.com>
2020-03-11 20:42:38 -0400

    update

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    var saveToWatchlist = function saveToWatchlist(imdbID) {
+        var movie = movieData.find(function(currentMovie) {
+           return currentMovie.imdbID == imdbID;
+        });
+
+     var watchlistJSON = localStorage.getItem('watchlist');
+     var watchlist = JSON.parse(watchlistJSON);  
+     if (watchlist === null)
+     return []; 
+
+     watchlist.push(movie);
+     watchlistJSON = JSON.stringify(watchlist);
+     localStorage.setItem('watchlist', watchlistJSON);
+     }; 
+
     var renderMovies = function renderMovies(movieArray) {
         // Creating a variable with a .map method (methods by default are always functions)
         moviesHTML = movieArray.map(function(currentMovie) {
-           
         return  `
         <div>
         <img src="${currentMovie.Poster}" width="150"/>
         <h3>${currentMovie.Title}</h3>
         <p>${currentMovie.Year}</p>
-        <button onclick="saveToWatchlist(${currentMovie.imdbID})">Add Movie</button> 
+        <button onclick="saveToWatchList(${currentMovie.imdbID})">Add Movie</button> 
         </div>
         ` 
     
@@ -23,25 +37,17 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         for (var i =0; i < 3; i++) {
         document.getElementsByClassName("movie")[i].innerHTML = renderMovies(movieData);
-        }; 
-    })
-    
-    } 
+        } 
+    })}; 
          
-    function saveToWatchlist(imdbID) {
-     var movie = movieData.find( function(currentMovie) {
-         currentMovie.imdbID == imdbID;
-     var watchlistJSON = localStorage.getItem('watchlist');
-     var watchlist = JSON.parse(watchlistJSON);  
-     if (watchlist === null)
-     return []; 
-     watchlist.push(movie);
-     watchlistJSON = JSON.stringify(watchlist);
-     localStorage.setItem('watchlist', watchlistJSON);
-     }); 
-    }
-// END OF RENDER MOVIES FUNCTION
-renderData();
-saveToWatchlist();
+    
+     
+     // END OF RENDER MOVIES FUNCTION
+     
+     renderData();
+    return saveToWatchlist;
+   
+ 
 
-})
+});
+
--- watchlist.js
+++ watchlist.js
@@ -1,9 +1,48 @@
 localStorage.getItem('watchlist');
 
+document.addEventListener('DOMContentLoaded', function() {
+    var saveToWatchlist = function saveToWatchlist(imdbID) {
+        var movie = movieData.find(function(currentMovie) {
+           return currentMovie.imdbID == imdbID;
+        });
 
-       var renderData = function renderData() { 
+     var watchlistJSON = localStorage.getItem('watchlist');
+     var watchlist = JSON.parse(watchlistJSON);  
+     if (watchlist === null)
+     return []; 
+
+     watchlist.push(movie);
+     watchlistJSON = JSON.stringify(watchlist);
+     localStorage.setItem('watchlist', watchlistJSON);
+     }; 
+
+    var renderMovies = function renderMovies(movieArray) {
+        // Creating a variable with a .map method (methods by default are always functions)
+        moviesHTML = movieArray.map(function(currentMovie) {
+           
+        return  `
+        <div>
+        <img src="${currentMovie.Poster}" width="150"/>
+        <h3>${currentMovie.Title}</h3>
+        <p>${currentMovie.Year}</p>
+        <button onclick="saveToWatchlist(${currentMovie.imdbID})">Add Movie</button> 
+        </div>
+        ` 
+    
+       }).join("")
+       return moviesHTML;
+    }
+
+    var renderData = function renderData() { 
         //    document.getElementById("moviesContainer").innerHTML = renderMovies(movieData) 
     document.getElementById("myForm").addEventListener('submit', function(e) {
         e.preventDefault();
-        document.getElementById("moviesContainer").innerHTML = renderMovies(movieData);
-    })} 
+        for (var i =0; i < 3; i++) {
+        document.getElementsByClassName("movie")[i].innerHTML = renderMovies(localStorage.getItem('watchlist'));
+        }; 
+        
+    })}
+    console.log(moviesHTML)
+    saveToWatchlist += renderData(); 
+    return renderData(); 
+})
