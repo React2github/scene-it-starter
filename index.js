a3074698534efb959af741640295f2aad2367b31
Abdella Shukur <abdella.shukur2@gmail.com>
2020-03-15 19:19:05 -0400

    added in api

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
         //    document.getElementById("moviesContainer").innerHTML = renderMovies(movieData) 
     document.getElementById("search-form").addEventListener('submit', function(e) {
         e.preventDefault();
-
+        var searchString = document.getElementById('search-bar').value 
+        axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=2d91e6e4',urlEncodedSearchString)
+        .then(function (response) { console.log(response.data)})        
+        var urlEncodedSearchString = encodeURIComponent(searchString);
         var movieContainer = document.getElementById("movieContainer")
          return movieContainer.innerHTML = renderMovies(movieData);
 
